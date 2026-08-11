d28b9ae185862277c23721641eb4d092dcdd02f0
valerie-design297 <valeriebykim@gmail.com>
2026-08-11 12:28:54 -0600

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,51 @@
+const memories = [
+  {
+    image: "photos/danpolaroid15.jpeg",
+    caption: "2nd date, kinda nervous. I think this is where our furniture obsession came from lol"
+  },
+
+  {
+    image: "photos/danpolaroid2.jpeg",
+    caption: "Proof of furniture obsession (IKEA date)"
+  },
+
+  {
+    image: "photos/danpolaroid3.jpeg",
+    caption: "You don't understand how insanely jealous I was that all the cats gravitated toward you. I think I fell harder for you in this moment because if cats liked you, that was a green flag in my book :)"
+  },
+
+  {
+    image: "photos/danpolaroid9.jpeg",
+    caption: "Cook Out run!!! And honorary Tar Heel for life??"
+  },
+
+  {
+    image: "photos/danpolaroid11.jpeg",
+    caption: "Chili's Triple Dipper + Cynthia Erivo margarita :D"
+  },
+
+  {
+    image: "photos/danpolaroid12.JPEG",
+    caption: "No explanation needed lol, I just like this pic of us go-karting"
+  },
+
+  {
+    image: "photos/danpolaroid13.jpeg",
+    caption: "I totally beat you btw (I did not)"
+  },
+
+  {
+    image: "photos/danpolaroid14.jpeg",
+    caption: "This is your contact pic in my phone"
+  },
+
+  {
+    image: "photos/danpolaroid4.jpeg",
+    caption: "Ski trip!!!! Not featured: my bruised knees"
+  },
+
+  {
+    image: "photos/danpolaroid6.JPG",
+    caption: "Soccer game 😼"
+  }
+];
