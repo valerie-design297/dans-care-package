f5ac3ebc9f29af005dbc6aa9fde3cd64f5f8dad4
valerie-design297 <valeriebykim@gmail.com>
2026-08-11 12:45:27 -0600

    Create awards.js

---
+++ awards.js
@@ -0,0 +1,49 @@
+const awards = [
+  {
+    icon: "🛋️",
+    title: "Best Furniture Shopping Companion",
+    text: "For outstanding dedication to looking at furniture we absolutely do not need."
+  },
+
+  {
+    icon: "🐈",
+    title: "Most Likely to Steal My Cats' Affection",
+    text: "Still bitter about this one."
+  },
+
+  {
+    icon: "🥸",
+    title: "Best Mustache",
+    text: "An objectively prestigious category."
+  },
+
+  {
+    icon: "🍸",
+    title: "Most Questionable Beverage Choice",
+    text: "The Cynthia Erivo margarita will not be forgotten."
+  },
+
+  {
+    icon: "🏎️",
+    title: "Second Best Go-Kart Driver",
+    text: "Awarded despite repeated allegations that you beat me."
+  },
+
+  {
+    icon: "🐏",
+    title: "Honorary Tar Heel",
+    text: "Citizenship granted indefinitely."
+  },
+
+  {
+    icon: "⛷️",
+    title: "Best Ski Trip Partner",
+    text: "My bruised knees declined to comment."
+  },
+
+  {
+    icon: "💗",
+    title: "My Favorite Person",
+    text: "For making the last year one of my favorites. This one's yours forever :)"
+  }
+];
